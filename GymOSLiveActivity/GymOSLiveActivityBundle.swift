04a0c6e7f58fad0532390e5c14ca61088a32288f
OJS26 <oliverstrachan_01@hotmail.co.uk>
2026-06-24 14:05:10 +0100

    Add Live Activity for active workout Dynamic Island

---
+++ GymOSLiveActivity/GymOSLiveActivityBundle.swift
@@ -0,0 +1,18 @@
+//
+//  GymOSLiveActivityBundle.swift
+//  GymOSLiveActivity
+//
+//  Created by OJ Strachan on 24/06/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct GymOSLiveActivityBundle: WidgetBundle {
+    var body: some Widget {
+        GymOSLiveActivity()
+        GymOSLiveActivityControl()
+        GymOSLiveActivityLiveActivity()
+    }
+}
